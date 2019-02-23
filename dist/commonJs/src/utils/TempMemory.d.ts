37dad5cdeb6498224e6b97ab765684baa6adc771
dennisat <info@anel.co>
2019-02-23 17:38:12 +0100

    last minute changes

--- dist/commonJs/src/utils/TempMemory.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TempMemory {
-    private mem;
-    delAll(): Promise<void>;
-    set(container: string, key: string, value: any): Promise<void>;
-    get(container: string, key: string): Promise<any>;
-    del(container: string, key: string): Promise<void>;
-}
