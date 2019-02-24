c1d7c2b6a015780c9704a6f99194d406236d4644
dennisat <info@anel.co>
2019-02-24 13:00:33 +0100

    last minute changes

--- dist/commonJs/isNode.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNode = !!(typeof process !== 'undefined' && process.versions && process.versions.node);
-//# sourceMappingURL=isNode.js.map
--- dist/commonJs/tests/index.d.ts
+++
@@ -1,5 +0,0 @@
-import "dyna-node-console";
-import "../dyna/mock-jest";
-import "../dyna/unhandledPromiseRejections";
-import "./scripts/priority.spec";
-import "./scripts/fast-entry.spec";
